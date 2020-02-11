f68557fa35dad44cc309ddb76b802aed391952f9
José Ignacio Gómez Quinteros <tgmelipilla@gmail.com>
2020-02-11 14:45:21 -0300

    First Project Test

---
+++ src/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,18 @@
+package miprimerprograma;
+
+public class MiPrimerPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		String a =new String("fire");
+//		String b = a;
+//		a = a.replace('f', 'd');
+//		System.out.printf(b);
+		
+		int i = 0;
+		while(i < 100)
+			System.out.println(++i);
+	}
+
+}
